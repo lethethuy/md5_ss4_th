b2bcfa482faec9646ac64cced921a3d6d560d59b
Lê Thế Thuỳ <you@example.com>
2023-09-06 07:48:49 +0700

    done

---
+++ src/main/java/ra/repository/ICustomerRepository.java
@@ -0,0 +1,10 @@
+package ra.repository;
+
+
+import ra.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
